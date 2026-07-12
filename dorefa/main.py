4a803ac12fd908638d20545bbb1aca0b3a80b005
Nitisarath <64013165+pllus@users.noreply.github.com>
2026-07-12 18:08:30 +0700

    feat:renamed added utils added main

--- dorefa/main.py
+++ dorefa/main.py
@@ -1,6 +1,23 @@
-#DoreFa quant Pipeline 
+#!/usr/bin/env python3
+import sys
+from resnet_dorefa import Model, run_image
+from tensorpack import SmartInit
 
 
 def main():
-    #Training loop here 
-    pass
+    # Configuration
+    MODEL_PATH = "ResNet-18-14f.npz"
+    IMAGES = [
+        "a.jpg",
+        "b.jpg",
+    ]
+
+    run_image(
+        model=Model(),
+        sess_init=SmartInit(MODEL_PATH),
+        inputs=IMAGES,
+    )
+
+
+if __name__ == "__main__":
+    main()
